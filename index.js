85702f1b1231e42e86efaa3a4e0204b1d57677ed
gamiacdev <43669816+gamiacdev@users.noreply.github.com>
2018-10-11 05:44:44 +0530

    suddenChange

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ con.connect(function(err) {
 });
  
 
-var server = app.listen(80,function () {
+var server = app.listen(3000,'127.0.0.1' ,function () {
     var host = server.address().address
     var port = server.address().port
     console.log("Server listening at http://%s:%s", host, port)
