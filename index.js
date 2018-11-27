d9d2558fdd94a19041d60b896d915e9af244b46e
Truskendev <44809194+Truskendev@users.noreply.github.com>
2018-11-27 13:04:05 +0530

    test.trusken

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ con.connect(function(err) {
 });
  
 var created=new Date();
-var server = app.listen(8080,function () {
+var server = app.listen(3000,'206.189.142.47' ,function (){
     var host = server.address().address
     var port = server.address().port
     console.log("Server listening at http://%s:%s", host, port)
